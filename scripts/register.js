bd1d5cc5eb9f73b2fe105c3a482aade835ba516b
abosmraa <samerfakih15@gmail.com>
2025-04-20 20:38:30 +0300

    get and sanitize user input from registration form

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+function registerUser() {
+    // Get and sanitize user input from registration form
+    const email = document.getElementById("email").value.trim().toLowerCase();
+    const password = document.getElementById("password").value;
+
+
+}
+
+
+
