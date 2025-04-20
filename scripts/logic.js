36eb23ca7d77af53d0d4c59dfd24199373a934b9
abosmraa <samerfakih15@gmail.com>
2025-04-20 20:14:17 +0300

    Add DOMContentLoaded listener and event binding

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,3 +1,13 @@
+// Add DOMContentLoaded listener and event binding
+document.addEventListener("DOMContentLoaded", function () {
+    const welcomebtn = document.getElementById("welcomeloginbtn")
+    if (welcomebtn) {
+        welcomebtn.addEventListener("click", goToResult)}
+    const loginbtn = document.getElementById("button")
+    if (loginbtn) {
+        loginbtn.addEventListener("click", checkLogin)
+    }
+});
 
 
 
