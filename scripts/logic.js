ff866866b70921daed7e2a54a719ff2e7e0e25c3
abosmraa <samerfakih15@gmail.com>
2025-04-20 20:16:59 +0300

    Implement login check using localStorage

--- scripts/logic.js
+++ scripts/logic.js
@@ -13,8 +13,22 @@ function goToResult() {
     console.log("Welcome Login button clicked");
     document.getElementById("loginForm").scrollIntoView({ behavior: "smooth" });
 }
+//  Implement login check using localStorage
+function checkLogin() {
+    const emailInput = document.getElementById("logEmail").value.trim().toLowerCase();
+    const passwordInput = document.getElementById("logPassword").value;
 
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
+    const validUser = users.find(user => user.email === emailInput && user.password === passwordInput);
+
+    if (validUser) {
+        alert("Login successful!");
+        location.href = "../home.html";
+    } else {
+        alert("Incorrect email or password.");
+    }
+}
 
 
 
