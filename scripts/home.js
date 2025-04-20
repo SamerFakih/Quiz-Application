cfc4c0dd6a55ed70b3d2311f832f8fb25774a66d
abosmraa <samerfakih15@gmail.com>
2025-04-20 21:00:56 +0300

    define quiz array with title,id and image path

---
+++ scripts/home.js
@@ -0,0 +1,7 @@
+// Define quiz data array with title, ID, and image path
+const quizTitle = [
+    { title: "HTML", id: "html", image: "../image/html-file_2786969.png" },
+    { title: "CSS", id: "CSS", image: "../image/css-document_8322479.png" },
+    { title: "JavaScript", id: "JS", image: "../image/javascript_10809622.png" }
+];
+
