0102b83ae019376dd5ea3d76a4a0c229e839d590
abosmraa <samerfakih15@gmail.com>
2025-04-21 19:19:27 +0300

    fixed opitions in question

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,19 +1,19 @@
 // Define questions for HTML, CSS, and JavaScript quizzes
 const htmlquiz = [
     { question: "What does HTML stand for?", option1: "Hyper Text Markup Language", option2: "Hyperlinks and Text Markup Language", option3: "Hyper Text Markup Link", answer: 1 },
-    { question: "What is the correct HTML element for the largest heading?", option1: "h6", option2: "heading", option3: "h1", answer: 3 },
-    { question: "What is the correct HTML element for inserting a line break?", option1: "break", option2: "br", option3: "lb", answer: 2 }
+    { question: "What is the correct HTML element for the largest heading?", option1: "&lt;h6&gt;", option2: "&lt;heading&gt;", option3: "&lt;h1&gt;", answer: 3 },
+    { question: "What is the correct HTML element for inserting a line break?", option1: "&lt;break&gt;", option2: "&lt;br&gt;", option3: "&lt;lb&gt;", answer: 2 }
 ];
 
 const cssquiz = [
     { question: "What does CSS stand for?", option1: "Cascading Style Sheets", option2: "Colorful Style Sheets", option3: "Computer Style Sheets", answer: 1 },
-    { question: "Which HTML tag is used to define an internal style sheet?", option1: "style", option2: "css", option3: "script", answer: 1 },
+    { question: "Which HTML tag is used to define an internal style sheet?", option1: "&lt;style&gt;", option2: "&lt;css&gt;", option3: "&lt;script&gt;", answer: 1 },
     { question: "Which property is used to change the background color?", option1: "bgcolor", option2: "background-color", option3: "color", answer: 2 }
 ];
 
 const jsquiz = [
     { question: "What does JS stand for?", option1: "JavaScript", option2: "JavaSource", option3: "JavaScriptSource", answer: 1 },
-    { question: "Which symbol is used for comments in JavaScript?", option1: "//", option2: "#", option3: "!-- --", answer: 1 },
+    { question: "Which symbol is used for comments in JavaScript?", option1: "//", option2: "#", option3: "&lt;!-- --&gt;", answer: 1 },
     { question: "Which event occurs when the user clicks on an HTML element?", option1: "onclick", option2: "onmouseover", option3: "onchange", answer: 1 }
 ];
 
@@ -41,9 +41,9 @@ quiz.forEach((q, index) => {
     container.innerHTML += `
         <div class="question-block">
             <p>${index + 1}. ${q.question}</p>
-            <label><input type="radio" name="q${index}" value="1"> ${q.option1}</label><br/>
-            <label><input type="radio" name="q${index}" value="2"> ${q.option2}</label><br/>
-            <label><input type="radio" name="q${index}" value="3"> ${q.option3}</label><br/>
+            <label><input type="radio" name="q${index}" value="1"> "${q.option1}"</label><br/>
+            <label><input type="radio" name="q${index}" value="2"> "${q.option2}"</label><br/>
+            <label><input type="radio" name="q${index}" value="3"> "${q.option3}"</label><br/>
         </div>
     `;
 });
